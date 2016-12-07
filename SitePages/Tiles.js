03dc986ac9d093772d56d3c8f9fc271392ec7391
vamseeachanta <vamsee.achanta@aceengineer.com>
2016-12-06 18:30:09 -0600

    Create Tiles.js
    2016-12-06 : Secondary JS File

---
+++ SitePages/Tiles.js
@@ -0,0 +1,6 @@
+function expandTile(id) {
+    document.getElementById(id).setAttribute('style', 'top:0px');
+}
+function condenseTile(id) {
+    document.getElementById(id).setAttribute('style', 'top:142px');
+}
